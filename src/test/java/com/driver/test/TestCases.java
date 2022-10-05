4fc05068df304b147ca926e2143df2219e3c7953
Chetan-0311 <107633040+Chetan-0311@users.noreply.github.com>
2022-10-05 13:54:56 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,18 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
+     class Product{
+           public int product(int x, int y, int z){
+                 return x * y * z;
+           }
+          public double product (double x, double y){
+                 return x * y;
+          }
+     }
      
+     public static void main(String[] args){
+          Product p = new Product();
+          p.product (5,6,7);
+          p.prosuct(5.5,6.6);
+     }     
 }
