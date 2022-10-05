34087082a218bb7a170c53c903377c392a7480f3
Chetan-0311 <107633040+Chetan-0311@users.noreply.github.com>
2022-10-05 13:59:44 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-
+     class Product{
+           public int product(int x, int y, int z){
+                 return x * y * z;
+           }
+          public double product(double x, double y){
+                 return x * y;
+          }
+     }
+     
+     public static void main(String[] args){
+          Product p = new Product();
+          p.product (5,6,7);
+          p.product(5.5,6.6);
+     } 
 }
